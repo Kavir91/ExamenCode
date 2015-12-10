56e39300e0cc505816290709905308760d7bcf03
redmo <redmo@192.168.1.105>
2015-12-10 14:13:03 -0600

    Aria Hidden

--- application/controllers/index_controller.php
+++ application/controllers/index_controller.php
@@ -24,6 +24,64 @@ class index_controller extends CI_Controller {
     public function index() {
         $this->load->view('index/index');
     }
+    public function ingresar()
+	{		
+		$this->load->view('headers/librerias');
+		$this->load->view('index');
+		$this->load->view('footer');
+	}
+    public function logueo()
+	{
+	    $data=array();
+		$query=$this->input->get('query', TRUE);
+		if($query)
+		{
+			$result=$this->index_model->logueo(trim($query));
+			if ($result!=FALSE)
+			{
+				$data=array('result'=>$result);
+				$this->load->view('headers/librerias');
+				$this->load->view('confirma_datos', $data);
+				$this->load->view('footer');
+				
+			}else{
+				$this->load->view('headers/librerias');
+				$this->load->view('index');
+				$this->load->view('footer');
+				echo "<p tabindex='0' role='alert'>Clave incorrecta, inténtalo de nuevo.</p>";
+			}
+		}
+	}
+
+	public function confirma()
+	{
+		$data=array();
+		$query=$this->input->get('query', TRUE);
+		$result=$this->index_model->logueo(trim($query));
+		$data=array('result'=>$result);
+		$this->load->view('headers/librerias');
+		$this->load->view('confirma_datos', $data);
+		$this->load->view('footer');
+			
+	}
+	public function creaExamen()
+	{
+		//date_default_timezone_set('America/Mexico_City');
+		//$fechadeExamen = date('Y-m-d');
+		//$horaIncio=date("H:i:s");
+		/*$data = array(
+			'idExamen' => $this ->input->post('idExamen', TRUE),
+			'fechadeExamen' => date('Y-m-d'),
+			'horaInicio' => date('H:i:s'),
+			'horaFinal' => date('H:i:s'),
+			'verExamen' => $this ->input->post('verExamen', TRUE),	
+			'aspirante_folioUV' => $this->input->post('aspirante_folioUV', TRUE),
+		);
+		$this->index_model->crear($data);	*/
+		//redirect('index_controller/preguntas');	
+		
+		redirect('/preguntas_controller/index/');
+	}
 
 }
 
